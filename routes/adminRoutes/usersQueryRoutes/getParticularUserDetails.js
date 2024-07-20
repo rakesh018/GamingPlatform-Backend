738dd96a834860e418b2a4a7c9d10bf39917e636
rakesh <rakhibodapatla@gmail.com>
2024-07-20 12:12:09 +0530

    added admin functionality to get details of particular user

---
+++ routes/adminRoutes/usersQueryRoutes/getParticularUserDetails.js
@@ -0,0 +1,33 @@
+const User = require("../../../models/userModels");
+
+const getParticularUserDetails = async (req, res) => {
+  try {
+    const userId = req.params?.userId;
+    console.log(userId);
+    if (!userId) {
+      throw new Error(
+        JSON.stringify({ status: 404, message: `INVALID USERID` })
+      );
+    }
+    const getUser = await User.findOne({_id:userId});
+    if (!getUser) {
+      throw new Error(
+        JSON.stringify({ status: 400, message: `INVALID USER ID` })
+      );
+    }
+    res
+      .status(200)
+      .json({ message: `User details fetched successfully.`, user: getUser });
+  } catch (error) {
+    let parsedError;
+    try {
+      parsedError = JSON.parse(error.message);
+    } catch (e) {
+      parsedError = { status: 500, message: `ERROR FETCHING USER DETAILS` };
+    }
+    console.error(`ERROR FETCHING USER DETAILS : ${error}`);
+    res.status(parsedError.status).json({ error: parsedError.message });
+  }
+};
+
+module.exports = getParticularUserDetails;
--- routes/adminRoutes/usersQueryRoutes/usersEntry.js
+++ routes/adminRoutes/usersQueryRoutes/usersEntry.js
@@ -4,8 +4,12 @@ const router = express.Router();
 const getAllUsers = require("./getAllUsers");
 const getActiveUsers = require("./getActiveUsers");
 const getUnverifiedUsers = require("./getUnverifiedUsers");
+const getParticularUserDetails=require("./getParticularUserDetails");
+
 router.get("/", getAllUsers);
 router.get("/active", getActiveUsers);
-router.get("/unverified",getUnverifiedUsers);
+router.get("/unverified", getUnverifiedUsers);
+router.get("/details/:userId",getParticularUserDetails);
+
 
 module.exports = router;
