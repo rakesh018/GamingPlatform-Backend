84aab9650a23c0f274738ce94f71dbb0cd5a30ea
rakesh <rakhibodapatla@gmail.com>
2024-07-21 11:16:55 +0530

    modified token middleware for ban check

--- middlewares/tokenMiddleware.js
+++ middlewares/tokenMiddleware.js
@@ -1,19 +1,27 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModels");
 
-const validateToken = (req, res, next) => {
-    const token = req.header('Authorization').split(' ')[1]; //'Bearer token
-    if (!token) {
-        return res.status(401).json({ error: 'ACCESS DENIED, NO TOKEN PROVIDED' });
-    }
+//This middleware verifies jwt token and then proceeds to confirm if user is not banned
+const validateToken = async (req, res, next) => {
+  const token = req.header("Authorization").split(" ")[1]; //'Bearer token
+  if (!token) {
+    return res.status(401).json({ error: "ACCESS DENIED, NO TOKEN PROVIDED" });
+  }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.userId = decoded.userId;
-        next();
-    } catch (err) {
-        console.log(`Error occured while validating token : ${err}`);
-        res.status(400).json({ error: 'INVALID TOKEN.PLEASE LOGIN' });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    //also check if user is not banned
+    const isBanned = await User.findOne({ _id: req.userId }).select("isRestricted");
+    if (isBanned.isRestricted) {
+      return res.status(400).json({ error: `ACCESS DENIED` });
+    } else {
+      next();
     }
+  } catch (err) {
+    console.log(`Error occured while validating token : ${err}`);
+    res.status(400).json({ error: "INVALID TOKEN.PLEASE LOGIN" });
+  }
 };
 
-module.exports=validateToken;
+module.exports = validateToken;
