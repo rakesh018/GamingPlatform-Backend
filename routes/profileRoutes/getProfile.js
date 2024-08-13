11156d604453ae77b4cb89eae7ef64d20899ac13
rakesh <rakhibodapatla@gmail.com>
2024-08-13 18:18:16 +0530

    sending total balance while fetching profile details

--- routes/profileRoutes/getProfile.js
+++ routes/profileRoutes/getProfile.js
@@ -1,21 +1,27 @@
-const User=require('../../models/userModels');
-const getProfile=async (req, res) => {
+const User = require("../../models/userModels");
+const getProfile = async (req, res) => {
+  const userId = req.userId; //got this from validate Token middleware
 
-    const userId = req.userId; //got this from validate Token middleware
-
-    try {
-        const getUserFromDB = await User.findById(userId).select('_id phone email uid balance withdrawableBalance referralCode');
-        if (!getUserFromDB) {
-            res.status(404).json({ error: 'USER NOT FOUND ERROR' });
-        }
-        const totalBalance=getUserFromDB.balance+getUserFromDB.withdrawableBalance;
-        res.status(200).json({
-            ...getUserFromDB._doc, // Spread the user data
-            balance: totalBalance // Add the total balance
-        });
-    } catch (err) {
-        console.log(`Error getting Profile Details : ${err}`);
-        res.status(500).json({ error: "Internal Server Error" });
+  try {
+    const getUserFromDB = await User.findById(userId).select(
+      "_id phone email uid balance withdrawableBalance referralCode"
+    );
+    if (!getUserFromDB) {
+      res.status(404).json({ error: "USER NOT FOUND ERROR" });
     }
-}
-module.exports=getProfile;
+    const totalBalance =
+      getUserFromDB.balance + getUserFromDB.withdrawableBalance;
+    res.status(200).json({
+      _id: getUserFromDB._id,
+      email: getUserFromDB.email,
+      phone: getUserFromDB.phone,
+      uid: getUserFromDB.uid,
+      balance: totalBalance,
+      referralCode: getUserFromDB.referralCode,
+    });
+  } catch (err) {
+    console.log(`Error getting Profile Details : ${err}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+module.exports = getProfile;
