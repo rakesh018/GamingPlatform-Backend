33eda11f35107baf4f7d57e2319af0cd2e398e08
rakesh <rakhibodapatla@gmail.com>
2024-08-15 16:44:27 +0530

    send total deposited amount,withdrawn amount for user

--- routes/adminRoutes/usersQueryRoutes/getParticularUserDetails.js
+++ routes/adminRoutes/usersQueryRoutes/getParticularUserDetails.js
@@ -1,16 +1,21 @@
 const User = require("../../../models/userModels");
-const mongoose=require('../../../models/db');
+const ManualDeposit = require("../../../models/manualDeposit");
+const Withdrawal = require("../../../models/withdrawal");
+const mongoose = require("../../../models/db");
 
 const getParticularUserDetails = async (req, res) => {
   try {
-    const userId = req.params?.userId; //can query using both _id and uid of user
+    const userId = req.params?.userId; // can query using both _id and uid of user
     if (!userId) {
       throw new Error(
         JSON.stringify({ status: 404, message: `INVALID USERID` })
       );
     }
-    //If the query parameter is objectId will use both else will use uid for searching
+
+    // Check if the userId is a valid ObjectId
     const isValidObjectId = mongoose.Types.ObjectId.isValid(userId);
+
+    // Fetch user by _id or uid
     const getUser = await User.findOne({
       $or: [...(isValidObjectId ? [{ _id: userId }] : []), { uid: userId }],
     });
@@ -20,9 +25,29 @@ const getParticularUserDetails = async (req, res) => {
         JSON.stringify({ status: 400, message: `INVALID USER ID` })
       );
     }
-    res
-      .status(200)
-      .json({ message: `User details fetched successfully.`, user: getUser });
+
+    // Fetch total deposits and total withdrawals for the user
+    const [totalDeposits, totalWithdrawals] = await Promise.all([
+      ManualDeposit.aggregate([
+        { $match: { uid: getUser.uid } },
+        { $group: { _id: null, totalDeposits: { $sum: "$amount" } } }
+      ]),
+      Withdrawal.aggregate([
+        { $match: { uid: getUser.uid ,status:'completed'} },
+        { $group: { _id: null, totalWithdrawals: { $sum: "$amount" } } }
+      ]),
+    ]);
+
+    const totalDepositAmount = totalDeposits.length > 0 ? totalDeposits[0].totalDeposits : 0;
+    const totalWithdrawalAmount = totalWithdrawals.length > 0 ? totalWithdrawals[0].totalWithdrawals : 0;
+
+    // Return user details along with total deposits and withdrawals
+    res.status(200).json({
+      message: `User details fetched successfully.`,
+      user: getUser,
+      totalDeposits: totalDepositAmount,
+      totalWithdrawals: totalWithdrawalAmount,
+    });
   } catch (error) {
     let parsedError;
     try {
