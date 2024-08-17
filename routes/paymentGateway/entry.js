4041cc4c443ea8fe33186b014c5cd66a79cbb3cd
rakesh <rakhibodapatla@gmail.com>
2024-08-17 16:27:32 +0530

    fetch upi details for user

--- routes/paymentGateway/entry.js
+++ routes/paymentGateway/entry.js
@@ -9,6 +9,7 @@ const generatePSU = require("./manualDeposit");
 const saveManualDepositKey = require("./saveManualDepositKey");
 const withdrawalRequest = require("./withdrawalRequest");
 const validateWithdrawalRequest = require("../../middlewares/validateWithdrawalRequest");
+const getUpiDetails = require("./getUpiDetails");
 
 //routes
 router.post("/auto-payment", validateToken, validateAutoPayment, makeAutoPayment);
@@ -16,5 +17,6 @@ router.post("/webhook",webHook);
 router.post("/manual-payment/generate-psu",validateToken,generatePSU);
 router.post("/manual-payment/save-key",validateToken,saveManualDepositKey);
 router.post("/withdrawal-request",validateToken,validateWithdrawalRequest,withdrawalRequest);
+router.get("/get-upi-details",validateToken,getUpiDetails);
 
 module.exports = router;
