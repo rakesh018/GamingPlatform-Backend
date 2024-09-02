b610b8ba0e012afdff2dd94479e7b889bef31bf0
rakesh <rakhibodapatla@gmail.com>
2024-09-03 01:40:22 +0530

    sending red dot status with profile

--- routes/profileRoutes/getProfile.js
+++ routes/profileRoutes/getProfile.js
@@ -1,4 +1,5 @@
 const User = require("../../models/userModels");
+const Notification=require('../../models/notificationModel')
 const getProfile = async (req, res) => {
   const userId = req.userId; //got this from validate Token middleware
 
@@ -9,6 +10,7 @@ const getProfile = async (req, res) => {
     if (!getUserFromDB) {
       res.status(404).json({ error: "USER NOT FOUND ERROR" });
     }
+    const redDot=await Notification.exists({userId:userId,hasSeen:false}); //check if any unread notification exists
     const totalBalance =
       getUserFromDB.balance + getUserFromDB.withdrawableBalance;
     res.status(200).json({
@@ -19,6 +21,7 @@ const getProfile = async (req, res) => {
       balance: totalBalance,
       withdrawableBalance:getUserFromDB.withdrawableBalance,
       referralCode: getUserFromDB.referralCode,
+      redDot
     });
   } catch (err) {
     console.log(`Error getting Profile Details : ${err}`);
